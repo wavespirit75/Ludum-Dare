767a75acba1fbc8d2a36b65cf633b9567697d564
Shiung <hh.shiung@gmail.com>
2015-12-13 16:18:46 +0800

    Initiated the Menu scene

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	public void LoadScene(int playerNumber) {
+//		GameManager.numberOfPlayers = playerNumber;
+		Application.LoadLevel(playerNumber);
+	}
+}
